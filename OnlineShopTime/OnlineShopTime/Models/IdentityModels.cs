f84c2f300ca9eb2a2835f46a1712b8f4e2394d5b
BreakTheScilence <moisenovicha@gmail.com>
2014-06-14 07:13:43 +0300

    Minimum work with DB. Just need to commit.
    Just need to commit to download new changes.

--- OnlineShopTime/OnlineShopTime/Models/IdentityModels.cs
+++ OnlineShopTime/OnlineShopTime/Models/IdentityModels.cs
@@ -5,13 +5,27 @@ namespace OnlineShopTime.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection")
+            : base("ShopOnlineDB")
         {
         }
+
+        protected override void OnModelCreating(System.Data.Entity.DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<IdentityUser>().ToTable("Users").Property(p => p.Id).HasColumnName("UserId");
+            modelBuilder.Entity<ApplicationUser>().ToTable("Users").Property(p => p.Id).HasColumnName("UserId");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("UsersRoles");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("UserExternalLogins");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("UserClaim");
+            modelBuilder.Entity<IdentityRole>().ToTable("Roles");
+            modelBuilder.Entity<Photo>().ToTable("UserPhotoes");
+        } 
     }
 }
---
+++ OnlineShopTime/OnlineShopTime/Models/Photo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShopTime.Models
+{
+    public class Photo
+    {
+        public string Id {get; set; }
+        public string photo { get; set; }
+    }
+}
