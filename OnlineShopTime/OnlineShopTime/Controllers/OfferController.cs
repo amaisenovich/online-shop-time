4acae6c3076812e554d38b077c91b1a17ad69454
Yunatan <unatan77@gmail.com>
2014-06-21 21:12:54 +0300

    Some finished work
    You now can create offers with images. Images FTW!

--- OnlineShopTime/OnlineShopTime/Controllers/OfferController.cs
+++ OnlineShopTime/OnlineShopTime/Controllers/OfferController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using OnlineShopTime.Models;
 using CloudinaryDotNet;
+using System.IO;
 
 namespace OnlineShopTime.Controllers
 {
@@ -30,17 +31,56 @@ namespace OnlineShopTime.Controllers
                 WWO = new WorkWithOffers(User.Identity.Name);
             Session["WWO"] = WWO;
             ViewBag.NameIsCorrect = true;
-            return View(new Offers(m_cloudinary));
+            return View(new Offers());
         }
 
         [HttpPost]
         public ActionResult Create(Offers newOffer)
         {
+            string defaultImage = null;
+
             WWO = (WorkWithOffers)Session["WWO"];
             newOffer = WWO.CompleteOfferWithData(newOffer);
+            newOffer.Photo1URL = imageURLs.Count > 0 ? imageURLs.Dequeue() : defaultImage;
+            newOffer.Photo2URL = imageURLs.Count > 0 ? imageURLs.Dequeue() : defaultImage;
+            newOffer.Photo3URL = imageURLs.Count > 0 ? imageURLs.Dequeue() : defaultImage;
+            newOffer.Photo4URL = imageURLs.Count > 0 ? imageURLs.Dequeue() : defaultImage;
             WWO.AddNewOffer(newOffer);
             Session["WWO"] = WWO;
+            imageURLs.Clear();
             return RedirectToAction("Index", "Home");
+        }
+
+        static Queue<string> imageURLs = new Queue<string>();
+
+        [HttpPost]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public void AddImageToOffer()
+        {
+            if (imageURLs.Count >= 4)
+                return;
+
+            var headers = HttpContext.Request.Headers;
+
+            string content = null;
+            using (StreamReader reader = new StreamReader(HttpContext.Request.InputStream))
+            {
+                content = reader.ReadToEnd();
+            }
+
+            if (String.IsNullOrEmpty(content)) return;
+
+            Dictionary<string, string> results = new Dictionary<string, string>();
+
+            string[] pairs = content.Split(new char[] { '&' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var pair in pairs)
+            {
+                string[] splittedPair = pair.Split('=');
+
+                results.Add(splittedPair[0], splittedPair[1]);
+            }
+
+            imageURLs.Enqueue(results["public_id"]);
         }
     }
 }
